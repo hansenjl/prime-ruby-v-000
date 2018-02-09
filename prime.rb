96aab640f992d919d0ec66bb05067f5f6b25a156
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-02-09 14:18:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(int)
   array_of_numbers = (int+1...-2).to_a if int < -1
     if int > -2 && int < 2
       return false
-    elsif array_of_numbers.any?{ |num| int%num ==0  }
+    elsif array_of_numbers.any?{ |num| array_of_numbers%num ==0  }
       return false
     else
       return true
